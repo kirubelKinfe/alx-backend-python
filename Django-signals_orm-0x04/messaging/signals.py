265ec3a042635e89303e0517b8d6a15d7b4b7637
Kirubel Kinfe <kirubelk@act.com.et>
2025-06-10 16:42:46 +0300

    delete signal added

--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,9 +1,12 @@
 
 # messaging/signals.py
 from datetime import timezone
-from django.db.models.signals import post_save, pre_save
+from django.db.models.signals import post_save, pre_save, post_delete
 from django.dispatch import receiver
 from .models import Message, Notification, MessageHistory
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 @receiver(post_save, sender=Message)
 def create_message_notification(sender, instance, created, **kwargs):
@@ -38,3 +41,21 @@ def log_message_edit(sender, instance, **kwargs):
                 instance.last_edited = timezone.now()
         except Message.DoesNotExist:
             pass  # New message being created
+
+
+
+@receiver(post_delete, sender=User)
+def cleanup_user_data(sender, instance, **kwargs):
+    """
+    Cleans up all user-related data when a user is deleted.
+    """
+    # Delete all messages where user is sender or receiver
+    Message.objects.filter(sender=instance).delete()
+    Message.objects.filter(receiver=instance).delete()
+    
+    # Delete all message history where user edited messages
+    MessageHistory.objects.filter(edited_by=instance).delete()
+    
+    # Delete all notifications where user is recipient or sender
+    Notification.objects.filter(recipient=instance).delete()
+    Notification.objects.filter(sender=instance).delete()        
--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -1,3 +1,17 @@
+# accounts/views.py
 from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect
+from django.contrib import messages
 
-# Create your views here.
+User = get_user_model()
+
+@login_required
+def delete_user(request):
+    if request.method == 'POST':
+        user = request.user
+        user.delete()  # This will trigger the post_delete signal
+        messages.success(request, 'Your account has been successfully deleted.')
+        return redirect('home')
+    return render(request, 'accounts/confirm_delete.html')
